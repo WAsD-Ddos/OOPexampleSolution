897798fead23d51731bd286769ae8ce0f8ed5264
Дима Д <mitja3375@gmail.com>
2025-04-07 17:37:39 +0300

    ex

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-#include "maint.h"
 #include "students.h"
 
+
+
+
+//void show(Student st) {
+//	cout << "The name pupil: " << st.name << endl;
+//	cout << "The age pupil: " << st.age << endl;
+//	cout << "The mark pupil: " << st.mark << endl;
+//	cout << "The alive pupil: " << (st.alive?"yes" : "no") << endl;
+//	cout << "_________________" << endl;
+//}
+
+void change(Student st) {
+	st.name = "Bog";
+	st.age = 19;
+	st.mark = 3.5;
+	st.alive = false;
+}
+
+
+
 int main() {
+
 	Student st1,st2;
 
 	st1.name = "Bogdan";
@@ -14,9 +34,20 @@ int main() {
 	st2.mark = 7.1;
 	st2.alive = true;
 
-	//string name = st1.mark > st2.mark ? st1.name : st2.name;
-	Student t= st1.mark > st2.mark ? st1: st2;
-	cout <<"The best pupil: " <<t.name << endl;
+	
+	  //string name = st1.mark > st2.mark ? st1.name : st2.name;
+	 //Student t= st1.mark > st2.mark ? st1: st2;
+	cout<<st1.getString()<<endl;
+	//cout<<st2.getString()<<endl;
+	 //cout <<"The best pupil: " <<t.name << endl;
+	cout << "___________________________" << endl;
+	//change(st1);
+	st1.name = "Bog";
+
+	cout << st1.getString() << endl;
+
+
+
 
 
 	return 0;
--- students.h
+++ students.h
@@ -7,4 +7,16 @@ public:
 	int age;
 	double mark;
 	bool alive;
+
+	string getString() {
+		string res = "The name pupil : " + name + "\n";
+		res += "The age pupil: " + to_string(age) + "\n";
+		res += "The mark pupil: " + to_string(mark) + "\n";
+		res += "The alive pupil:";
+		res += alive ? "yes" : "no";
+		res += "\n";
+		return res;
+	}
+
 };
+
